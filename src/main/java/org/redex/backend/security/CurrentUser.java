c4e571a967b674166fdbbe68fa17805f66dbc10d
Johann Morales <johann.morales@pucp.edu.pe>
2018-10-11 11:46:41 -0500

    update

--- src/main/java/org/redex/backend/security/CurrentUser.java
+++ src/main/java/org/redex/backend/security/CurrentUser.java
@@ -7,10 +7,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
-@Target({ElementType.PARAMETER, ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
 @Documented
 @AuthenticationPrincipal
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER, ElementType.TYPE})
 public @interface CurrentUser {
 
 }
--- src/main/java/org/redex/backend/security/CustomUserDetailsService.java
+++ src/main/java/org/redex/backend/security/CustomUserDetailsService.java
@@ -1,19 +1,19 @@
 package org.redex.backend.security;
 
-import org.redex.model.Usuario;
+import org.redex.backend.repository.UsuariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.redex.backend.repository.UsuarioRepository;
+import org.redex.model.Usuario;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
-    UsuarioRepository usuariosRepository;
+    UsuariosRepository usuariosRepository;
 
     @Override
     @Transactional
