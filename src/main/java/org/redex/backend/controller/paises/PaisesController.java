b08072e612dfb205ecb50718f8a85170d9a7fae4
Johann Morales <johann.morales@pucp.edu.pe>
2018-10-23 11:21:55 -0500

    ipdate

--- src/main/java/org/redex/backend/controller/paises/PaisesController.java
+++ src/main/java/org/redex/backend/controller/paises/PaisesController.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.redex.backend.controller.paises;
 
-/**
- *
- * @author jmorales
- */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("paises")
 public class PaisesController {
     
 }
--- src/main/java/org/redex/backend/controller/paises/PaisesService.java
+++ src/main/java/org/redex/backend/controller/paises/PaisesService.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.redex.backend.controller.paises;
 
-/**
- *
- * @author jmorales
- */
-public class PaisesService {
+public interface PaisesService {
     
 }
