acaff065bf93bfaa3fc05bc3c93b795ea6e4c8e6
Johann Morales <johann.morales@pucp.edu.pe>
2018-10-23 10:39:59 -0500

    update

--- src/main/java/org/redex/backend/controller/usuarios/UsuariosController.java
+++ src/main/java/org/redex/backend/controller/usuarios/UsuariosController.java
@@ -1,29 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.redex.backend.controller.usuarios;
 
-import org.redex.backend.controller.oficinas.OficinasService;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.redex.backend.zelper.response.CargaDatosResponse;
 import org.redex.model.general.Archivo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author Oscar
- */
 @RestController
 @RequestMapping("/usuarios")
 public class UsuariosController {
+
     @Autowired
     UsuariosService usuariosService;
-    
+
+    @GetMapping
+    public ArrayNode list() {
+        
+    }
+
     @PostMapping("/carga")
     public CargaDatosResponse carga(@RequestBody Archivo archivo) {
         return usuariosService.carga(archivo);
