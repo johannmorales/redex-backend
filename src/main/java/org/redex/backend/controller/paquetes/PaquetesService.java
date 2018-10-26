8d6722a60f0c2a65521dc301b00cf68dbcd134c4
Johann Morales <johann.morales@pucp.edu.pe>
2018-10-26 05:58:38 -0500

    fix

--- src/main/java/org/redex/backend/controller/paquetes/PaquetesService.java
+++ src/main/java/org/redex/backend/controller/paquetes/PaquetesService.java
@@ -1,5 +1,10 @@
 package org.redex.backend.controller.paquetes;
 
+import java.util.List;
+import org.redex.backend.model.envios.Paquete;
+
 public interface PaquetesService {
+
+    public List<Paquete> list();
 
 }
