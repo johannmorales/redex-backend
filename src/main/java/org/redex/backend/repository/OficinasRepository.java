10c0ba91a17a17886a8beeb96790973e2be849de
Johann Morales <johann.morales@pucp.edu.pe>
2018-10-27 14:57:19 -0500

    busqueda oficinas

--- src/main/java/org/redex/backend/repository/OficinasRepository.java
+++ src/main/java/org/redex/backend/repository/OficinasRepository.java
@@ -3,6 +3,8 @@ package org.redex.backend.repository;
 import java.util.List;
 import org.redex.backend.model.general.EstadoEnum;
 import org.redex.backend.model.rrhh.Oficina;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,11 +16,14 @@ public interface OficinasRepository extends JpaRepository<Oficina, Long> {
 
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Oficina o set o.capacidadActual = o.capacidadActual + 1 where o.id = :#{#oficina.id}")
-    public void incrementarCapacidadActual(@Param("oficina") Oficina oficina);
-    
+    void incrementarCapacidadActual(@Param("oficina") Oficina oficina);
+
     public List<Oficina> findAllByEstado(EstadoEnum estado);
-    
-    @Query("select o from Oficina o join o.pais p where o.codigo like %:q% or p.codigo like %:q% or p.nombre like %:q%") 
-    public List<Oficina> customSearch(@Param(value = "q") String q);
-    
+
+    @Query("select o from Oficina o join o.pais p where o.codigo like %:q% or p.codigo like %:q% or p.nombre like %:q%")
+    List<Oficina> customSearch(@Param(value = "q") String q);
+
+    @Query("select o from Oficina o join o.pais p where o.codigo like %:q% or p.codigo like %:q% or p.nombre like %:q%")
+    Page<Oficina> customPaginatedSearch(@Param(value = "q") String q, Pageable pageable);
+
 }
--- src/main/java/org/redex/backend/zelper/Laboratory.java
+++ src/main/java/org/redex/backend/zelper/Laboratory.java
@@ -1,11 +1,19 @@
 package org.redex.backend.zelper;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Laboratory {
 
-//    public static void main(String[] args) {
-//        BCryptPasswordEncoder en = new BCryptPasswordEncoder();
-//        
-//        System.out.println(en.encode("dp1"));
-//
-//    }
+    public static void main(String[] args) {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+        LocalDateTime time = LocalDateTime.parse("2018-12-12 23:02", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        time = time.plus(-5, ChronoUnit.HOURS);
+        System.out.println(ZonedDateTime.of(time, ZoneId.of("UTC")));
+    }
 }
