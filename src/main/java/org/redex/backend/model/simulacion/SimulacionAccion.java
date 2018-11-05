3f807584eecab04fe7f9544294ae6ca1308ca6c9
Johann Morales <johann.morales@pucp.edu.pe>
2018-11-04 20:57:37 -0500

    fix

--- src/main/java/org/redex/backend/model/simulacion/SimulacionAccion.java
+++ src/main/java/org/redex/backend/model/simulacion/SimulacionAccion.java
@@ -2,16 +2,7 @@ package org.redex.backend.model.simulacion;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "simulacion_accion")
@@ -21,25 +12,25 @@ public class SimulacionAccion implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_simulacion", nullable = false)
     private Simulacion simulacion;
 
-    @ManyToOne
-    @JoinColumn(name = "id_oficina_llegada", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_oficina_llegada")
     private SimulacionOficina oficinaOrigen;
 
-    @ManyToOne
-    @JoinColumn(name = "id_oficina_salida", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_oficina_salida")
     private SimulacionOficina oficinaDestino;
 
     @Enumerated(EnumType.STRING)
     private SimulacionAccionTipoEnum tipo;
 
-    @Column(nullable = false)
+    @Column
     private LocalDateTime fechaInicio;
 
-    @Column(nullable = false)
+    @Column
     private LocalDateTime fechaFin;
 
     @Column(nullable = false)
