320225953e1895f41a5d890d754556a42051068e
Johana Gamboa <johana27094@hotmail.com>
2018-11-12 11:45:43 -0500

    fix find usuario

--- src/main/java/org/redex/backend/controller/usuarios/UsuariosController.java
+++ src/main/java/org/redex/backend/controller/usuarios/UsuariosController.java
@@ -102,6 +102,7 @@ public class UsuariosController {
                 "id",
                 "colaborador.*",
                 "colaborador.persona.*",
+                "colaborador.oficina.*",
                 "rol.*",
         });
     }
