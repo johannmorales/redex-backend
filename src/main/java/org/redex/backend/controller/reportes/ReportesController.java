93c80baf6d786803b0d513b70d1f355a0bd63f42
omcl1302 <a20121302@pucp.pe>
2018-11-15 07:32:33 -0500

    excel

--- src/main/java/org/redex/backend/controller/reportes/ReportesController.java
+++ src/main/java/org/redex/backend/controller/reportes/ReportesController.java
@@ -6,10 +6,24 @@
 package org.redex.backend.controller.reportes;
 
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.redex.backend.controller.archivos.ArchivosController;
 import org.redex.backend.controller.personas.PersonasService;
 import org.redex.backend.model.envios.VueloAgendado;
+import org.redex.backend.model.general.Archivo;
 import org.redex.backend.model.general.Persona;
+import org.redex.backend.zelper.exception.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,24 +35,93 @@ import pe.albatross.zelpers.miscelanea.JsonHelper;
  */
 public class ReportesController {
     
+    private static final Logger logger = LogManager.getLogger(ReportesController.class);
+    
     @Autowired
     ReportesService service;
     
     @PostMapping("/reportes/paquetesXvuelo")
-    public ResponseEntity<?> paquetesXvuelo(@RequestBody VueloAgendado va){
-        service.paquetesXvuelo(va.getId());
-        return null;
+    public ResponseEntity<Resource> paquetesXvuelo(@RequestBody VueloAgendado va){
+        Archivo archivo = service.paquetesXvuelo(va.getId());
+        try {
+            
+           Path path = Paths.get(archivo.getDirectorio())
+                .toAbsolutePath().normalize();
+
+           Path filePath = path.resolve(archivo.getNombreServidor()).normalize();
+           
+           Resource resource = new UrlResource(filePath.toUri());
+           
+            if (!resource.exists()) {
+                throw new MyFileNotFoundException("Archivo no encontrado " + archivo.getNombreOriginal());
+            }
+            
+            String contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + archivo.getNombreOriginal()+ "\"")
+                    .body(resource);
+
+        } catch (MalformedURLException ex) {
+            throw new MyFileNotFoundException("Archivo no encontrado " , ex);
+        }
     }
     
     @PostMapping("/reportes/paquetesXusuario")
-    public ResponseEntity<?> paquetesXusuario(@RequestBody Persona p){
-        service.paquetesXusuario(p.getId());
-        return null;
+    public ResponseEntity<Resource> paquetesXusuario(@RequestBody Persona p){
+        Archivo archivo = service.paquetesXusuario(p.getId());
+        try {
+            
+           Path path = Paths.get(archivo.getDirectorio())
+                .toAbsolutePath().normalize();
+
+           Path filePath = path.resolve(archivo.getNombreServidor()).normalize();
+           
+           Resource resource = new UrlResource(filePath.toUri());
+           
+            if (!resource.exists()) {
+                throw new MyFileNotFoundException("Archivo no encontrado " + archivo.getNombreOriginal());
+            }
+            
+            String contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + archivo.getNombreOriginal()+ "\"")
+                    .body(resource);
+
+        } catch (MalformedURLException ex) {
+            throw new MyFileNotFoundException("Archivo no encontrado " , ex);
+        }
     }
     
     @PostMapping("/reportes/enviosXfechas")
-    public ResponseEntity<?> enviosXfechas(@RequestBody RangoFechas r){
-        service.enviosXfechas(r.fecha_ini,r.fecha_fin);
-        return null;
+    public ResponseEntity<Resource> enviosXfechas(@RequestBody RangoFechas r){
+        Archivo archivo = service.enviosXfechas(r.fecha_ini,r.fecha_fin);
+        try {
+            
+           Path path = Paths.get(archivo.getDirectorio())
+                .toAbsolutePath().normalize();
+
+           Path filePath = path.resolve(archivo.getNombreServidor()).normalize();
+           
+           Resource resource = new UrlResource(filePath.toUri());
+           
+            if (!resource.exists()) {
+                throw new MyFileNotFoundException("Archivo no encontrado " + archivo.getNombreOriginal());
+            }
+            
+            String contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + archivo.getNombreOriginal()+ "\"")
+                    .body(resource);
+
+        } catch (MalformedURLException ex) {
+            throw new MyFileNotFoundException("Archivo no encontrado " , ex);
+        }
     }
 }
