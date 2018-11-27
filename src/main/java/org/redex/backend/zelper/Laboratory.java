5ed0f63ba9e64b642aec6c6d1d2b7cdfc5d0df4c
Oscar <Oscar@DESKTOP-44C66HF>
2018-11-27 18:00:14 -0500

    s

--- src/main/java/org/redex/backend/zelper/Laboratory.java
+++ src/main/java/org/redex/backend/zelper/Laboratory.java
@@ -1,20 +1,10 @@
 package org.redex.backend.zelper;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-<<<<<<< HEAD
-
-=======
->>>>>>> 48751837c36ccba3bc944f906c54eb281b2e8920
 
 public class Laboratory {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        
-=======
-        //BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        //System.out.println(encoder.encode("dp1"));
->>>>>>> 48751837c36ccba3bc944f906c54eb281b2e8920
+
     }
 
 }
