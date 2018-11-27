523c1ba5061aa57ba226b9770e3a1fb4042371a7
Oscar <Oscar@DESKTOP-44C66HF>
2018-11-27 17:59:15 -0500

    cron

--- src/main/java/org/redex/backend/controller/scheduledService/ScheduledServiceController.java
+++ src/main/java/org/redex/backend/controller/scheduledService/ScheduledServiceController.java
@@ -3,26 +3,19 @@ package org.redex.backend.controller.scheduledService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-<<<<<<< HEAD
-=======
 
->>>>>>> da85ec90504cfb6735f832f93d38305be33f004b
 @Component
 public class ScheduledServiceController {
     
     @Autowired
     ScheduledServiceService service;
     
-<<<<<<< HEAD
-    @Scheduled(fixedRate=60000)
-=======
-    @Scheduled(fixedRate=10000)
->>>>>>> da85ec90504cfb6735f832f93d38305be33f004b
+    @Scheduled(fixedRate=100000)
     public void despachoVuelo(){
         
-        //service.llegadaVuelos();
+        service.llegadaVuelos();
         //generarRutas();
-        //service.salidaVuelos();
+        service.salidaVuelos();
         
         System.out.println("hola");
     }
--- src/main/java/org/redex/backend/zelper/Laboratory.java
+++ src/main/java/org/redex/backend/zelper/Laboratory.java
@@ -1,10 +1,12 @@
 package org.redex.backend.zelper;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 public class Laboratory {
 
     public static void main(String[] args) {
-
+        
     }
 
 }
