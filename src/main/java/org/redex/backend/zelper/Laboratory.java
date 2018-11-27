9064fb3bb60c86f5a0a1aa3f7ffacba8ebf62d01
Johann Morales <johann.morales@pucp.edu.pe>
2018-11-27 12:02:54 -0500

    doom

--- src/main/java/org/redex/backend/zelper/Laboratory.java
+++ src/main/java/org/redex/backend/zelper/Laboratory.java
@@ -3,15 +3,8 @@ package org.redex.backend.zelper;
 
 public class Laboratory {
 
-    //private static ScheduledServiceController ssc; 
+    public static void main(String[] args) {
 
-
-//
-//    public static void main(String[] args) throws Exception {
-//    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//
-//        System.out.println(encoder.encode("dp1"));
-//
-//    }
+    }
 
 }
